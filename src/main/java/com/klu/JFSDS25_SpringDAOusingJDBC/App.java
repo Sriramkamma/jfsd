9984b44c201b1bba1aa9da38f796515665342edc
sriram <sriramkamma03@gmail.com>
2024-12-06 21:58:39 +0530

    springboot

---
+++ src/main/java/com/klu/JFSDS25_SpringDAOusingJDBC/App.java
@@ -0,0 +1,10 @@
+package com.klu.JFSDS25_SpringDAOusingJDBC;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
